32fab7965d88c9f5a3047cd564f00e53c134cc16
Esteban Dorado <fadoro@gmail.com>
2017-02-19 21:05:51 +0100

    Updated tests

--- e2e_test.py
+++ e2e_test.py
@@ -6,4 +6,4 @@ HOST='http://api-website-159123.appspot.com'
 
 response = urllib2.urlopen("{}/".format(HOST))
 html = response.read()
-assert(html == "Hello World!")
+assert(html == "Hello, World!")
--- main_test.py
+++ main_test.py
@@ -9,7 +9,7 @@ class MainTest(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/')
-        assert rv.data == 'Hello world!'
+        assert rv.data == 'Hello, World!'
 
 if __name__ == '__main__':
     unittest.main()
