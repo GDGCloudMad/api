18dfc67680159771d59b4e32d09e4794c5fb6cc1
Esteban Dorado <fadoro@gmail.com>
2017-02-21 20:34:25 +0100

    Updated projecttt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import urllib2
+import logging
 import json
 from google.appengine.ext import vendor
 vendor.add('lib')
@@ -9,6 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500    
 
 if __name__ == '__main__':
     app.run(debug=True)
